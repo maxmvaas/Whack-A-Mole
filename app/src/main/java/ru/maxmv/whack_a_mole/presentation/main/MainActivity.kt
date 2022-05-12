e89dcdebb16ff818f62c2aa614f63135e9b9cb92
maxmv <maxmvaas@gmail.com>
2022-05-12 15:04:55 +0400

    add ui for main menu

--- app/src/main/java/ru/maxmv/whack_a_mole/presentation/main/MainActivity.kt
+++ app/src/main/java/ru/maxmv/whack_a_mole/presentation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.maxmv.whack_a_mole
+package ru.maxmv.whack_a_mole.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.maxmv.whack_a_mole.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
