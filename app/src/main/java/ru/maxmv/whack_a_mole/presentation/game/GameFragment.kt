573fdc1d2fe72b8160025d94da732c1aa321b46a
maxmv <maxmvaas@gmail.com>
2022-05-12 18:43:00 +0400

    add logic for game [small fixes]

--- app/src/main/java/ru/maxmv/whack_a_mole/presentation/game/GameFragment.kt
+++ app/src/main/java/ru/maxmv/whack_a_mole/presentation/game/GameFragment.kt
@@ -7,11 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.os.HandlerCompat.postDelayed
+
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
+
 import kotlinx.coroutines.*
+
 import ru.maxmv.whack_a_mole.data.model.Hole
 import ru.maxmv.whack_a_mole.databinding.FragmentGameBinding
 import ru.maxmv.whack_a_mole.presentation.game.adapter.HoleAdapter
@@ -42,10 +43,8 @@ class GameFragment : Fragment() {
         binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
         binding.recyclerView.addItemDecoration(Decorator(50))
         var score = 0
-        binding.textViewHighscore.setText("$score")
+        binding.textViewHighscore.text = "$score"
         adapter.setItems(holes)
-
-
 
         binding.buttonPlay.root.setOnClickListener {
 
